d7ccbbd3f118efcb6a8406514b3461e6d91126e5
Yann PIQUET <yann.piquet@inference-operations.com>
2017-10-30 12:25:57 +0100

    Fix packages list and update version

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(
     name='django_rest_generators',
-    packages=['django_rest_generators'],
-    version='0.1.4',
+    packages=find_packages(),
+    version='0.1.5',
     description='Simple tool to add a bunch of commands related to django_rest_framework, django_guardian and behave',
     author='Yann Piquet',
     author_email='yann.piquet@epitech.eu',
@@ -14,7 +15,7 @@ setup(
     ],
     license='MIT',
     url='https://github.com/Exanis/django-rest-generators',
-    download_url='https://github.com/Exanis/django-rest-generators/archive/0.1.4.tar.gz',
+    download_url='https://github.com/Exanis/django-rest-generators/archive/0.1.5.tar.gz',
     keywords=['testing', 'django', 'djangorestframework', 'behave'],
     classifiers=[],
     include_package_data=True
